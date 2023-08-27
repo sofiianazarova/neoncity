b07b095b8766b659de0e5926143edf053469358f
Alexander Tuzhik <alprog@hotmail.com>
2023-08-28 00:47:27 +0200

    sprite

---
+++ Assets/Scripts/AnimationData.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class AnimationData
+{
+    [SerializeField] public Texture Texture;
+    [SerializeField] public float FrameRate;
+    [SerializeField] public float Speed;
+}
---
+++ Assets/Scripts/AnimationType.cs
@@ -0,0 +1,8 @@
+
+public enum AnimationType
+{
+    Idle,
+    Walk,
+    Run,
+    Jump
+}
